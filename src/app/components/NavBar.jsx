efd0336d5ae13596f2a9d3f26f467b096854d998
Simon <simon.lowe90@hotmail.co.uk>
2024-09-16 13:38:48 +0100

    Committing

---
+++ src/app/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function NavBar() {
+    return (
+        <nav>
+            <Link href="/all-anime">
+                Anime
+            </Link>
+        </nav>
+    )
+}
